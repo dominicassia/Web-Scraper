170ad314e095de6930aaf895a7205834dd6e1cba
dominic assia <dominicassia@gmail.com>
2020-12-26 21:33:16 -0500

    Minor fixes

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ headless        = True
 scrape_website  = 'https://www.nike.com/'
 
 # Webhook
-webhook_url     = ''
+webhook_url     = 'https://discord.com/api/webhooks/783445859147841586/fHFjOvhPUa4-eBc79J2AKG1cZpB9hKMRdeDJBORB0yZrr3h4kY37CMKDY8DLPBYGfvuB'
