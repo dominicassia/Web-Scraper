07cc00e5eccf01778f8814e913c73902a39b106d
Dominic Assia <dominicassia@gmail.com>
2022-08-22 14:26:57 -0400

    cleaned up comments and structure

--- app.py
+++ app.py
@@ -7,16 +7,26 @@
     using beautiful soup to parse the html; extracting the title.  
 '''
 
+# Project
 from config import *
+from bot import activate
+
+# General
+import time
+import multiprocessing
+import json
+import requests
+from datetime import datetime
+
+# Webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+# --- // --- #
 
 def get_driver():
     ''' Returns a webdriver. Headless and chrome bin as specified in config var'''
-
-    # Imports
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from config import headless, chrome_bin, chrome_path
 
     log.log('info', 'Getting driver')
 
@@ -43,8 +53,9 @@ def get_driver():
     log.log('info', 'Done')
     return driver
 
+
 def get_title(driver, scrape_website):
-    from selenium.webdriver.common.by import By
+    ''' Get title given Chrome driver '''
 
     log.log('info', f'Fetching: {scrape_website}')
 
@@ -65,68 +76,47 @@ def get_title(driver, scrape_website):
     log.log('info', 'Done')
     return title
 
-# Depreciated 
-def get_source(driver):
-    ''' Returns the page source. Website as specified in config variable. '''
-    from config import scrape_website
-
-    driver.implicitly_wait(5)
-    driver.get(scrape_website)
-    page_source = driver.page_source
-    driver.quit()
-    return page_source
-
-# Depreciated 
-def parse(page_source):
-    ''' Utilizes bs4 to parse page source. Returns source's title. '''
-    from bs4 import BeautifulSoup
-
-    soup = BeautifulSoup(page_source, 'html.parser')
-    title = soup.find('title')
-    return title
-
 
 def save_json(title):
     ''' Saves the title to a json file. Returns nothing. '''
-    import json
-    from datetime import datetime
-    from config import json_file_path
 
     log.log('info', 'Saving JSON')
 
     try:
+        # Open existing file in append mode
         with open(json_file_path, 'r+') as fr:
+
             data = dict(json.load(fr))
             data[ str(datetime.now()) ] = str(title)
-            with open(json_file_path, 'w') as fw:
-                json.dump(data, fw)
-                log.log('info', 'Written')
 
+            log.log('info', 'Data written')
+
+    # Log file DNE 
     except FileNotFoundError:
+
+        # Create new log file
         with open(json_file_path, 'r+') as fw:
+
             dictionary = {}
             dictionary[ str(datetime.now()) ] = title
             json.dump(dictionary, fw)
+
             log.log('info', 'New JSON file created')
-    
     log.log('info', 'Done')
 
 
 def send_webhook(title):
     ''' Utilizes requests to send webhook. Returns nothing. '''
 
-    import json
-    import requests
-    from datetime import datetime
-    from config import data_webhook_url
-
     log.log('info', 'Sending webhook')
 
+    # Create JSON structure
     data = {
         'username'  : 'Web Scraper',
         'content'   : f'[ {str(datetime.now())} ] - {title}'
     }
 
+    # POST
     requests.post(
         data_webhook_url, 
         data        = json.dumps(data), 
@@ -137,38 +127,35 @@ def send_webhook(title):
 
 
 def main(website=scrape_website):
+
     log.log('info', 'Launching web scraper')
 
+    # Begin execution
     try:
         driver = get_driver()
         title = get_title(driver, website)
-        # page_source = get_source(driver)
-        # title = parse(page_source)
         save_json(title)
         send_webhook(title)
 
         log.log('info', 'Scrape complete')
 
+    # Catch and log exception
     except Exception as e:
         log.log('error', 'Exception raised:')
         log.log('error', e)
-        pass
 
-# ----
+# --- // --- #
 
 if __name__ == "__main__":
-    import time, multiprocessing
-    from config import *
-    from bot import activate
-
-
+    ''' On file call '''
+    
     log.log('info', 'Initializing app')
 
     p1 = multiprocessing.Process(name='p1', target=main)
     p2 = multiprocessing.Process(name='p2', target=activate)
     p2.start()
 
-    # Repeat this process every day
+    # Repeat this process every day -- migrate to PaaS scheduler
     while True:
         p1.start()
 
