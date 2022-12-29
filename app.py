2aab2437843b9f095cb7b387a0e4c8426129c3ce
Dominic Assia <dominicassia@gmail.com>
2022-12-29 10:06:12 -0500

    finalized restructure

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-'''
-# Web Scraper
+###################################
+# Imports
+###################################
+from models import WebScraper
+from os import environ as env
 
-
-
-'''
-
+###################################
+# Set all configuration variables here
+###################################
 class Config():
 
     def __init__(self):
-        # Set all configuration variables here
 
         # Log of web scraper status
         self.run_log_file_path = ''
@@ -21,100 +22,18 @@ class Config():
         # Discord bot
         self.bot_token = ''
 
+        # Chrome driver
+        self.chrome_binary_path = ''
+        #self.chrome_binary_path = env.get('GOOGLE_CHROME_BIN')
+        self.chrome_path = ''
+        #self.chrome_path = env.get('CHROMEDRIVER_PATH')
+
+        self.headless = True
+
+
 ###################################
-# Run app on 
+# Run app on file call
 ###################################
 if __name__ == '__main__':
 
-
-
-
-
-
-
-def save_json(title):
-    ''' Saves the title to a json file. Returns nothing. '''
-
-    log.log('info', 'Saving JSON')
-
-    try:
-        # Open existing file in append mode
-        with open(json_file_path, 'r+') as fr:
-
-            data = dict(json.load(fr))
-            data[ str(datetime.now()) ] = str(title)
-
-            log.log('info', 'Data written')
-
-    # Log file DNE 
-    except FileNotFoundError:
-
-        # Create new log file
-        with open(json_file_path, 'r+') as fw:
-
-            dictionary = {}
-            dictionary[ str(datetime.now()) ] = title
-            json.dump(dictionary, fw)
-
-            log.log('info', 'New JSON file created')
-    log.log('info', 'Done')
-
-
-def send_webhook(title):
-    ''' Utilizes requests to send webhook. Returns nothing. '''
-
-    log.log('info', 'Sending webhook')
-
-    # Create JSON structure
-    data = {
-        'username'  : 'Web Scraper',
-        'content'   : f'[ {str(datetime.now())} ] - {title}'
-    }
-
-    # POST
-    requests.post(
-        data_webhook_url, 
-        data        = json.dumps(data), 
-        headers     = {'Content-Type': 'application/json'}
-    )
-
-    log.log('info', 'Done')
-
-
-def main(website=scrape_website):
-
-    log.log('info', 'Launching web scraper')
-
-    # Begin execution
-    try:
-        driver = get_driver()
-        title = get_title(driver, website)
-        save_json(title)
-        send_webhook(title)
-
-        log.log('info', 'Scrape complete')
-
-    # Catch and log exception
-    except Exception as e:
-        log.log('error', 'Exception raised:')
-        log.log('error', e)
-
-############################
-# Web Scraper
-############################
-if __name__ == "__main__":
-
-
-
-    log.log('info', 'Initializing app')
-
-    p1 = multiprocessing.Process(name='p1', target=main)
-    p2 = multiprocessing.Process(name='p2', target=activate)
-    p2.start()
-
-    # Repeat this process every day -- migrate to PaaS scheduler
-    while True:
-        p1.start()
-
-        log.log('info', 'Sleeping...')  
-        time.sleep(60*60)
+    ws = WebScraper()
--- models.py
+++ models.py
@@ -1,96 +1,31 @@
+###################################
+# Imports
+###################################
 import time, datetime, requests, json, multiprocessing, discord, discord.ext
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-from app import Config 
-
-
-###################################
-# Primary: Web Scraper class
-###################################
-class WebScraper():
-
-    def __init__(self):
-
-        
-
-        # Initialize bot
-        self.bot = 
-
-        # Get driver
-        self.driver = self.__get_driver()
-        
-
-
-    def __get_driver(self):
-        self.log.log('info', 'Getting driver')
-
-        # Chrome webdriver options
-        chrome_options = Options()
-        chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-        chrome_options.binary_location = chrome_bin
-
-        # Prooduction
-        if headless:
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-
-        # Debug
-        else:
-            chrome_options.add_argument('start-maximized')
-
-        # Create webdriver
-        service = Service(executable_path=chrome_path)
-        driver = webdriver.Chrome(service=service, options=chrome_options)
-        
-        log.log('info', 'Done')
-        return driver
-
-
-    def scrape(self, url):
-        self.log.log('info', f': {url}')
-
-        self.driver.implicitly_wait(5)
-        self.driver.get(url) 
-        self.driver.implicitly_wait(5)
-
-        log.log('info', f'Actual: {driver.current_url}')
-        log.log('info', 'Fetching title')
-
-        title = driver.title
-
-        log.log('info', f'Title: {title}')
-        
-        driver.implicitly_wait(5)
-        driver.quit()
-
-        log.log('info', 'Done')
-        return title
-
-
+from app import Config
 
 ###################################
 # Component: Logger class
 ###################################
 class Logger():
 
-    def __init__(self, log_fp, log_webhook):
+    def __init__(self):
 
-        self.log_fp = log_file_path
-        self.webhook_url = log_webhook_url
+        self.log_fp = config.log_file_path
+        self.webhook_url = config.log_webhook_url
 
         # Attempt to open log file
         try:
-            with open(log_fp, 'r') as f:
+            with open(self.log_fp, 'r') as f:
                 pass
 
         # Create the log file
         except FileNotFoundError:
-            with open(log_fp, 'w') as f:
+            with open(self.log_fp, 'w') as f:
                 self.log('info', 'Log file created')
 
     def log(self, type, msg):
@@ -123,6 +58,146 @@ class Logger():
 
 
 ###################################
+# Global Components: Logger and Config classes
+###################################
+logger = Logger()
+config = Config()
+
+###################################
+# Primary: Web Scraper class
+###################################
+class WebScraper():
+
+    def __init__(self):
+
+        logger.log('info', 'Initializing web scraper app')
+
+        # Initialize Bot
+        self.bot = Bot()
+
+        # Create process
+        self.bot_process = multiprocessing.Process(name='bot_process', target=self.bot.activate)
+
+        # Get driver
+        self.driver = self.__get_driver()
+
+        # Schedule
+        self.repeat = None
+        
+        # Start bot process
+        self.bot_process.start()
+
+
+    def __get_driver(self):
+        self.log.log('info', 'Getting driver')
+
+        # Chrome webdriver options
+        chrome_options = Options()
+        chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+        chrome_options.binary_location = config.chrome_binary_path
+
+        # Prooduction
+        if config.headless:
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+
+        # Debug
+        else:
+            chrome_options.add_argument('start-maximized')
+
+        # Create webdriver
+        service = Service(executable_path=config.chrome_path)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        
+        self.logger.log('info', 'Done')
+        return driver
+
+
+    def save_json(self, payload):
+        logger.log('info', 'Saving JSON')
+
+        try:
+            # Open existing file in append mode
+            with open(config.json_file_path, 'r+') as fr:
+
+                data = dict(json.load(fr))
+                data[ str(datetime.now()) ] = str(payload)
+
+                logger.log('info', 'Data written')
+
+        # Log file DNE 
+        except FileNotFoundError:
+
+            # Create new log file
+            with open(config.json_file_path, 'r+') as fw:
+
+                dictionary = {}
+                dictionary[ str(datetime.now()) ] = payload
+                json.dump(dictionary, fw)
+
+                logger.log('info', 'New JSON file created')
+        logger.log('info', 'Done')
+
+
+    def send_webhook(self, payload):
+        logger.log('info', 'Sending webhook')
+
+        # Create JSON structure
+        data = {
+            'username'  : 'Web Scraper',
+            'content'   : f'[ {str(datetime.now())} ] - {payload}'
+        }
+
+        # POST
+        requests.post(
+            config.data_webhook_url, 
+            data        = json.dumps(data), 
+            headers     = {'Content-Type': 'application/json'}
+        )
+
+        logger.log('info', 'Done')
+
+
+    def scrape_utility(self, url):
+        self.log.log('info', f': {url}')
+
+        self.driver.implicitly_wait(5)
+        self.driver.get(url) 
+        self.driver.implicitly_wait(5)
+
+        logger.log('info', f'Actual: {self.driver.current_url}')
+        logger.log('info', 'Fetching title')
+
+        title = self.driver.title
+
+        logger.log('info', f'Title: {title}')
+        
+        self.driver.implicitly_wait(5)
+        self.driver.quit()
+
+        logger.log('info', 'Done')
+
+        self.send_webhook(title)
+        self.save_json(title)
+
+
+    def scrape(self, url):
+        # Create process
+        self.ws_process =  multiprocessing.Process(name='ws_process', target=self.scrape_utility, args=(url,))
+
+        while True:
+            self.ws_process.start()
+            
+            if self.repeat:
+                logger.log('info', 'Sleeping...')  
+                time.sleep(self.repeat)
+            else:
+                break
+
+
+###################################
 # Component: (Discord) Bot
 ###################################
 class Bot(Logger):
@@ -131,10 +206,10 @@ class Bot(Logger):
         self.client = discord.ext.commands.Bot(command_prefix='>')
 
     def activate(self):
-        self.client.run(Config.bot_token)
+        self.client.run(config.bot_token)
 
 
-@client.event
+@Bot.client.event
 async def on_ready():
     print('[INFO] Bot is ready.')
 
@@ -144,15 +219,25 @@ async def view(ctx, *, file_path):
 
     try:
         await ctx.send(file=discord.File(file_path))
-        log.log('info', 'Done.')
+        logger.log('info', 'Done.')
 
     except Exception as e:
-        log.log('error', 'Exception raised:')
-        log.log('error', e)
+        logger.log('error', 'Exception raised:')
+        logger.log('error', e)
         pass
 
+@Bot.client.command()
+async def set_repeat(ctx, *, repeat):
+    logger.log('bot', f'Set repeat {repeat}')
+    try:
+        WebScraper.repeat = int(repeat)
+
+    except Exception as e:
+        logger.log('error', 'Exception raised:')
+        logger.log('error', e)
+        pass
 
 @Bot.client.command()
 async def scrape(ctx, *, website):
-    log.log('bot', f'Scrape {website}')
-    main(website)
+    logger.log('bot', f'Scrape {website}')
+    WebScraper.scrape(website)
